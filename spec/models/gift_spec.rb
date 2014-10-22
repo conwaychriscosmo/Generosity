03dd4d1cfb19627058b7538d3106c943250e19f5
Chris Conway <conwayc@berkeley.edu>
2014-10-22 11:18:30 -0700

    added some gift creation unit tests

--- spec/models/gift_spec.rb
+++ spec/models/gift_spec.rb
@@ -1,5 +1,24 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe Gift do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Gift, :type => :model  do
+#  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "validations" do
+    before(:each) do
+      Gift.delete_all
+    end
+
+    it "should output the gift name and url" do
+      output = Gift.create('cat', 'google.com')
+      hsh = { errCode: 1, name: 'cat', url: 'google.com') }
+      expect(output).to eq hsh
+    end
+
+    it "should enforce short names" do
+      longname = 'a'*129
+      output = Gift.create(longname, 'a.con')
+      hsh = { errCode: -1 }
+      expect(output).to eq hsh
+    end 
+  end    
 end
