0548e650720fcae302c6e41b8100e0a3a2a1a6fb
Chris Conway <conwayc@berkeley.edu>
2014-10-22 23:11:44 -0700

    challenge tests run and fail

--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -1,5 +1,30 @@
-require 'spec_helper'
+require 'rails_helper'
+require 'users'
 
-describe Challenge do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Challenge, :type => :model  do
+#  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    before(:each) do
+      Gift.delete_all
+      Users.delete_all
+      Challenge.delete_all
+    end
+
+    it "should output a challenge given users" do
+      Users.add('fred', 'iloveme')
+      Users.add('george', 'notfromharrypotter')
+      output = Challenge.match('george')
+      hsh = { errCode: 1, Giver: 'george', Recipient: 'fred' }
+      expect(output).to eq hsh
+    end
+
+    it "should throw an error given no users" do
+      Users.add('greg', 'verykinky')
+      output = Challenge.match('greg')
+      hsh = {errCode: -1 }
+      expect(output).to eq hsh
+    end
+  end
+
 end
