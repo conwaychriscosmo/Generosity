084f076de88ee35267af4cf98f31bcc93bb24454
tg-galeforce <josephtwli@berkeley.edu>
2014-11-07 07:51:04 -0800

    Temporarily commented out offending challenge code in backend controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
             username = params[:username]
 			user = Users.find_by(username: username)
 			login(user)
-			challenge = Challenge.match(username)
-			challengeCode = challenge[:errCode]
-			data[:challengeCode] = challengeCode
+			# challenge = Challenge.match(username)
+			# challengeCode = challenge[:errCode]
+			# data[:challengeCode] = challengeCode
 		end
 		render json: data
 	end
