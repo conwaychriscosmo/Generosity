91cc9813976fbdd2cf5c8e8c9db37e47363b64a5
sollipse <solipsistic@berkeley.edu>
2014-11-07 20:38:45 -0800

    Frontend tests delviered

--- spec/requests/frontpage_spec.rb
+++ spec/requests/frontpage_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 describe 'home page' do
-	before(:each) do
-		visit '/'
+	before (:each) do 
+		visit "/"
 	end
   it 'Loads the actual html' do
     page.should have_content('Generosity')
+  end
+
+  it 'displays shakespeare if challenge is clicked', :js => true do
+  	click_link('Challenge')
+  	page.should have_content('Midsummer Night')
+  end
+
+  it 'redirects to tracker if map is clicked', :js => true do
+  	url =URI.parse(current_url)
+  	click_link('Map')
+  	URI.parse(current_url).should eq(url[0..-3]+"/tracker")
   end
 
   it 'Correctly loads the navbar.', :js => true do
@@ -14,46 +25,83 @@ describe 'home page' do
   it 'loads the js.', :js => true do
   	page.should have_content('Sign up now!')
   end
-
-  it 'validates for empty username.', :js => true do
-  	click_link('Sign up now!')
-  	click_button('Create Account')
-  	puts page.driver.browser.switch_to.alert.text.should eq("Error: The username is empty, too long, or has invalid characters.")
-  end
-
-  it 'validates for long username.', :js => true do
-  	click_link('Sign up now!')
-  	fill_in "Username", with: "a"*101
-  	click_button('Create Account')
-  	puts page.driver.browser.switch_to.alert.text.should eq("Error: The username is empty, too long, or has invalid characters.")
-  end
-
-  it 'validates for no password.', :js => true do
-  	click_link('Sign up now!')
-  	fill_in "Username", with: "Nucker Dan"
-  	click_button('Create Account')
-  	puts page.driver.browser.switch_to.alert.text.should eq("Error: The password is empty, too long, or has invalid characters.")
-  end
-
-	it 'validates for long password.', :js => true do
-  	click_link('Sign up now!')
-  	fill_in "Username", with: "Asshole Bob"
-  	fill_in "Password", with: "a"*100
-  	fill_in "Real Name", with: "OOOH AH"
-  	fill_in "6pm-6am", with: "1-9"
-  	fill_in "Berkeley, California", with: "Berk"
-  	click_button('Create Account')
-  	puts page.driver.browser.switch_to.alert.text.should eq("Error: The password is empty, too long, or has invalid characters.")
-  end
-
   it 'creates a user given good input', :js => true do
+  	
   	click_link('Sign up now!')
-  	fill_in "Username", with: "Asshole Bob"
+  	value = ""; 8.times{value  << (65 + rand(25)).chr}
+  	fill_in "Username", with: value
   	fill_in "Password", with: "haha!fuku"
   	fill_in "Real Name", with: "Hank Hardon"
   	fill_in "6pm-6am", with: "None"
   	fill_in "Berkeley, California", with: "Hell"
   	click_button('Create Account')
-  	puts page.driver.browser.switch_to.alert.text.should eq("User created.")
+  	page.driver.browser.switch_to.alert.text.should eq("User created.")
   end
+
+  it 'informs you when empty username is unacceptable', :js => true do
+  	click_link('Sign up now!')
+  	value = ""; 8.times{value  << (65 + rand(25)).chr}
+  	fill_in "Password", with: "haha!fuku"
+  	fill_in "Real Name", with: "Hank Hardon"
+  	fill_in "6pm-6am", with: "None"
+  	fill_in "Berkeley, California", with: "Hell"
+  	click_button('Create Account')
+  	page.driver.browser.switch_to.alert.text.should eq("Error: The username is empty, too long, or has invalid characters.")
+  end
+
+  it 'informs you when empty password is unacceptable', :js => true do
+  	click_link('Sign up now!')
+  	value = ""; 8.times{value  << (65 + rand(25)).chr}
+  	fill_in "Username", with: "haha!fuku"
+  	fill_in "Real Name", with: "Hank Hardon"
+  	fill_in "6pm-6am", with: "None"
+  	fill_in "Berkeley, California", with: "Hell"
+  	click_button('Create Account')
+  	page.driver.browser.switch_to.alert.text.should eq("Error: The password is empty, too long, or has invalid characters.")
+  end
+
+  it 'informs you when empty real name is unacceptable', :js => true do
+  	click_link('Sign up now!')
+  	value = ""; 8.times{value  << (65 + rand(25)).chr}
+  	fill_in "Username", with: "haha!fuku"
+  	fill_in "password", with: "HankHardon"
+  	fill_in "6pm-6am", with: "None"
+  	fill_in "Berkeley, California", with: "Hell"
+  	click_button('Create Account')
+  	page.driver.browser.switch_to.alert.text.should eq("Error: The real name is empty, too long, or has invalid characters.")
+  end
+
+  it 'doesnt log you in with only username', :js => true do
+  	click_link("Log in")
+  	fill_in "Username", with: "Hahayousuck"
+  	page.driver.browser.switch_to.alert.text.should eq("Login failed.")
+  end
+
+  it 'doesnt log you in with only username', :js => true do
+  	click_link("Log in")
+  	fill_in "Username", with: "Hahayousuck"
+  	page.driver.browser.switch_to.alert.text.should eq("Login failed.")
+  end
+
+  it 'doesnt log you in with no credentials', :js => true do
+  	click_link("Log in")
+  	page.driver.browser.switch_to.alert.text.should eq("Login failed.")
+  end
+
+  it 'logs you in if you exist', :js => true do
+  	click_link('Sign up now!')
+  	value = ""; 8.times{value  << (65 + rand(25)).chr}
+  	fill_in "Username", with: value
+  	fill_in "Password", with: "haha!fuku"
+  	fill_in "Real Name", with: "Hank Hardon"
+  	fill_in "6pm-6am", with: "None"
+  	fill_in "Berkeley, California", with: "Hell"
+  	click_button('Create Account')
+  	click_link("Log in")
+  	fill_in "Username", with: value
+  	fill_in "Password", with: "haha!fuku"
+  	click_button("Log in")
+  	page.driver.browser.switch_to.alert.text.should eq("Login succeeded.")
+  end
+
 end
