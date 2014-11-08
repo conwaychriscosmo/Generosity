3bf6005b12b02f0786e5f3ca420869ebefc43a01
Greg Monson <gregorymonson@gmail.com>
2014-11-07 20:03:25 -0800

    factory girl stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,6 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
     	login user
     	data[:errCode] = 1
-      session[:user_id] = user.id
     else
       data[:errCode] = -1
     end
