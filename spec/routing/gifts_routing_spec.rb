35ec75b0c58eb3a4f8d21295d4d76e105a5344f1
Nick <nhelvig@berkeley.edu>
2014-11-21 16:26:28 -0800

    Added gift routing tests

--- spec/routing/gifts_routing_spec.rb
+++ spec/routing/gifts_routing_spec.rb
@@ -3,6 +3,30 @@ require "spec_helper"
 describe GiftsController do
   describe "routing" do
 
+    post 'gifts/:id/rate', to: 'gifts#rate'
+  post 'gifts/:id/review', to: 'gifts#review'
+  post 'gifts/:id/deliver', to: 'gifts#deliver'
+  post 'gifts/:id/delete', to: 'gifts#delete'
+  post 'challenge/:id/delete', to: 'challenges#delete'
+  get 'gifts/:id/view', to: 'gifts#view'
+
+    it "routes to #rate" do
+      post("/gifts/1/rate").should route_to("gifts#rate", :id => "1")
+    end
+    it "routes to #review" do
+      post("/gifts/1/review").should route_to("gifts#review", :id => "1")
+    end
+    it "routes to #deliver" do
+      post("/gifts/1/deliver").should route_to("gifts#deliver", :id => "1")
+    end
+    it "routes to #delete" do
+      post("/gifts/1/delete").should route_to("gifts#delete", :id => "1")
+    end   
+
+    it "routes to #view" do
+      post("/gifts/1/view").should route_to("gifts#view", :id => "1")
+    end   
+
     it "routes to #index" do
       get("/gifts").should route_to("gifts#index")
     end
@@ -28,6 +52,10 @@ describe GiftsController do
     end
 
     it "routes to #destroy" do
+      post("/gifts").should route_to("gifts#destroy")
+    end
+
+    it "routes to #destroy with id" do
       delete("/gifts/1").should route_to("gifts#destroy", :id => "1")
     end
 
