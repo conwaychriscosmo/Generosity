6581f95aad20bfa616e902443b31819c9d818a82
sollipse <solipsistic@berkeley.edu>
2014-11-23 21:32:08 -0800

    Gift form changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'challenges/complete' => 'challenge#complete', as: :complete
   post 'gifts/create' => 'gifts#create'
   post 'gifts/destroy' => 'gifts#destroy'
+  post 'gifts/find_user' => 'gifts#find_user'
   post 'challenge/complete' => 'challenges#complete'
   post 'challenge/joinQueue' => 'challenges#joinQueue'
   post 'challenge/onQueue' => 'challenges#onQueue'
